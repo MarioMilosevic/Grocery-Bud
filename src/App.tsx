98e5b60bcf6d862c25ae95122a87941e04d89aae
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-24 15:29:50 +0100

    Add Grocery Bud div

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
-
 function App() {
-
   return (
     <>
-     
+      <div className="border border-black bg-white w-[560px] mx-auto">
+      <h1 className="">Grocery Bud</h1>
+    <div>
+      <input type="text" />
+      <button>Add Item</button>
+    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
