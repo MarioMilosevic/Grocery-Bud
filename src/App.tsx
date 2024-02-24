1e957b2f344c7c11debece2e5d0eac684f53bf9d
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-24 15:42:08 +0100

    Finish css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
   return (
     <>
-      <div className="border border-black bg-white w-[560px] mx-auto">
-      <h1 className="">Grocery Bud</h1>
-    <div>
-      <input type="text" />
-      <button>Add Item</button>
+      <div className="bg-white w-[460px] mx-auto pt-8 pb-10 px-8">
+      <h1 className="pb-4 text-2xl">Grocery Bud</h1>
+    <div className="flex justify-between rounded-lg">
+      <input className="bg-slate-100 w-[70%] px-2 text-sm rounded-md" type="text" />
+      <button className="w-[30%] bg-blue-300 text-white text-sm py-1 rounded-md">Add Item</button>
     </div>
       </div>
     </>
