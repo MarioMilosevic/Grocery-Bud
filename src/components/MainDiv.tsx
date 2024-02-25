c0db66dd807fe28a8dcbf1ebedf490a61186105f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-25 12:55:47 +0100

    Add todos

--- src/components/MainDiv.tsx
+++ src/components/MainDiv.tsx
@@ -1,44 +1,47 @@
 import Todo from "./Todo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type TodoState = {
   text: string;
   checked: boolean;
 };
 const MainDiv = () => {
-  const [todo, setTodo] = useState<TodoState>({
-    text: "",
-    checked: false,
-  });
+  const [todo, setTodo] = useState<TodoState>();
   const [todos, setTodos] = useState([]);
 
-  const createTodo = () => {
-    const input = document.querySelector("input");
-    const inputValue = input.value;
-    setTodo({
-      text: inputValue,
-      checked: false,
-    });
-    console.log(todo)
+  useEffect(() => {}, [todo]);
+
+  const createTodo = (e) => {
+    setTodo({ text: e.target.value, checked: false, id:crypto.randomUUID() })
+  }
+  
+
+  const updateTodos = (e) => {
+    e.preventDefault()
+    setTodos([...todos, todo]);
+    setTodo({})
   };
 
   return (
     <div className="bg-white w-[460px] mx-auto pt-8 pb-10 px-8 rounded-md shadow-sm hover:shadow-lg transition duration-300">
       <h1 className="pb-4 text-2xl text-center">Grocery Bud</h1>
-      <div className="flex justify-between rounded-lg pb-6">
+      <form
+        className="flex justify-between rounded-lg pb-6"
+      >
         <input
           className="bg-slate-100 w-[70%] px-2 text-sm rounded-md"
           type="text"
+          onChange={createTodo}
         />
         <button
           className="w-[30%] bg-blue-300 text-white text-sm py-1 rounded-md"
-          onClick={createTodo}
+          onClick={updateTodos}
         >
           Add Item
         </button>
-      </div>
+      </form>
       {todos.map((todo) => {
-        return <Todo text={todo.text} checked={todo.checked} key={todo} />;
+        return <Todo text={todo.text} checked={todo.checked} key={todo.id} />;
       })}
     </div>
   );
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,9 @@
-const Todo = () => {
+const Todo = ({text}) => {
   return (
     <article className="flex justify-between">
       <div className="flex gap-2">
         <input type="checkbox" />
-        <p className="text-sm">Odje ce da primi stejt preko propsa</p>
+        <p className="text-sm">{text}</p>
       </div>
       <button className="text-sm bg-gray-900 text-white px-1 rounded-sm">
         Delete
